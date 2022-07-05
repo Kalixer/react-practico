38aec8862ab163302bbf069403e2e83cf4dec6b9
Kalixer <al.caceres.ag72@gmail.com>
2022-07-05 18:49:04 -0400

    Images and alias on Webpack and some integration

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,49 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-        publicPath: '/',
-    },
-    mode: 'development',
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)?$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                }
-            },
-            {
-                test: /\.html$/,
-                use: {
-                    loader: 'html-loader',
-                }
-            },
+    devtool: 'source-map',
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js',
+		publicPath: '/'
+	},
+	mode: 'production',
+	resolve: {
+		extensions: ['.js', '.jsx'],
+		alias: {
+			'@components': path.resolve(__dirname, '/src/components/'),
+			'@containers': path.resolve(__dirname, '/src/containers/'),
+			'@pages': path.resolve(__dirname, '/src/pages/'),
+			'@icons': path.resolve(__dirname, '/src/assets/icons/'),
+			'@logos': path.resolve(__dirname, '/src/assets/logos/'),
+			'@styles': path.resolve(__dirname, '/src/styles/'),
+			'@routes': path.resolve(__dirname, '/src/routes/'),
+		}
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader'
+				}
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				type: 'asset',
+			},
+			{
+				test: /\.html$/,
+				use: [
+					{
+						loader: 'html-loader'
+					}
+				]
+			},
 			{
 				test: /\.(css|scss)$/,
 				use: [
@@ -35,27 +51,19 @@ module.exports = {
 					"css-loader",
 					"sass-loader",
 				],
-			},
-			{
-				test: /\.(png|svg|jpg|gif)$/,
-				type: 'asset'
-			},
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                type: 'asset',
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './public/index.html',
-            filename: './index.html'
-        }),
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './public/index.html',
+			filename: './index.html'
+		}),
 		new MiniCssExtractPlugin({
 			filename: '[name].css'
 		}),
-    ],
+	],
 	devServer: {
 		historyApiFallback: true,
-    }
+	}
 }
